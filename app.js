6b58315998b34f4304cda35fac24a39eca0a448e
Karlito14 <leiroz26@hotmail.com>
2023-05-02 10:33:09 +0200

    création de la BDD et installation de mongoose

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://leiroz26:monVieuxGrimoire052023@bddmonvieuxgrimoire.rnsqoxc.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use('/api/books', (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
+
+app.get('/api/books', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({ message: 'objet créé' });
+})
 
 app.get('/api/books', (req, res, next ) => {
     const books = [
