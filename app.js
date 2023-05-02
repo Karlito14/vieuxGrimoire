1f29d487e5962d638d0f3644bc28b2069ffe05d8
Karlito14 <leiroz26@hotmail.com>
2023-05-02 12:14:10 +0200

    création de lensemble des routes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
 const app = express();
 const mongoose = require('mongoose');
+const Book = require('./models/Book');
 
 mongoose.connect('mongodb+srv://leiroz26:monVieuxGrimoire052023@bddmonvieuxgrimoire.rnsqoxc.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -18,45 +18,47 @@ app.use('/api/books', (req, res, next) => {
     next();
 });
 
-app.get('/api/books', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({ message: 'objet créé' });
-})
+app.post('/api/books', (req, res, next) => {
+    const book = new Book({
+        ...req.body
+    });
+    book.save()
+    .then(() => res.status(201).json({ message: 'Livre ajouté' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.post('/api/books/:id/rating', (req, res, next) => {
+    
+});
+
+app.put('/api/books/:id', (req, res, next) => {
+    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre mis à jour !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/books/:id', (req, res, next) => {
+    Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/books/:id', (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/books/bestrating', (req, res, next) => {
+    Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
+});
 
 app.get('/api/books', (req, res, next ) => {
-    const books = [
-        {
-            userId : 'id1250',
-            title : 'a la peche',
-            author : 'montessori',
-            year : 2000,
-            imageUrl : 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            genre : 'roman',
-            ratings : [
-                {
-                    userId : 'id1250',
-                    grade : 5,
-                }
-            ],
-            averageRating : 4,
-        },
-        {
-            userId : 'id1240',
-            title : 'a la montagne',
-            author : 'montessori',
-            year : 2002,
-            genre : 'roman',
-            imageUrl : 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            ratings : [
-                {
-                    userId : 'id1240',
-                    grade : 4,
-                }
-            ],
-            averageRating : 4,
-        }
-    ];
-    res.status(200).json(books);
+    Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
